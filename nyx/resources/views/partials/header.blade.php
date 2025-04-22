9d16c40316897570f3a3313e8de65299c2f6cffe
xkmetk <145667362+xkmetk@users.noreply.github.com>
2025-04-22 17:35:19 +0200

    first blades + app layout

--- nyx/resources/views/partials/header.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="top-ribbon"></div>
-
-<div class="container-fluid top-bar-white">
-    <div class="container py-2 d-flex justify-content-end align-items-center">
-        <a href="{{ url('/login') }}" class="sign-link">Sign In / Sign Up</a>
-    </div>
-</div>
-

2e02585c3dcacef889539f953ff2deb3880fd8ca
David Vach <david.vach32@gmail.com>
2025-04-29 15:52:24 +0200

    admin mode

---
+++ nyx/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-4">
+        <h1>Admin Dashboard</h1>
+        <p>You are logged in as administrator.</p>
+    </div>
+@endsection
